aeb7277ced3858ac09a4e4a8516d21acdf7590ee
Beder Acosta Borges <bedermail@yahoo.com>
2024-02-10 15:52:12 +0100

    Define models

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,21 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AppController', () => {
   let app: TestingModule;
+  const userService = {
+    users: jest.fn(() => [{ email: 'test@example.com' }]),
+  };
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       controllers: [AppController],
-      providers: [AppService],
-    }).compile();
+      providers: [PrismaService, UserService],
+    })
+      .overrideProvider(UserService)
+      .useValue(userService)
+      .compile();
   });
 
   describe('getHello', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello World!"', async () => {
       const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(await appController.getHello()).toBe(
+        `Hello ${userService.users()[0].email}!`,
+      );
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { UserService } from './user.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly userService: UserService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const users = await this.userService.users({ take: 1 });
+
+    return users && users.length ? `Hello ${users[0].email}!` : 'Hello World!';
   }
 }
